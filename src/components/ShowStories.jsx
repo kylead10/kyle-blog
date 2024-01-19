77276d07368a4713d80f65e5e590432c724e2aa1
kylead10 <adiokayode111@gmail.com>
2024-01-19 20:24:54 +0100

    update

---
+++ src/components/ShowStories.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Story from './Story';
+import useDataFetcher from '../hooks/dataFetcher';
+import Loader from './Loader';
+
+const ShowStories = ({ type }) => {
+  const { isLoading, stories = [] } = useDataFetcher(type ? type : 'top');
+  return (
+    <React.Fragment>
+      <Loader show={isLoading}>Loading...</Loader>
+      <React.Fragment>
+        {stories.map(
+          (story) => story && <Story key={story.id} story={story} />
+        )}
+      </React.Fragment>
+    </React.Fragment>
+  );
+};
+
+export default ShowStories;
