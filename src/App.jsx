28681ce16771bdded2ccf9590f30e6226e6ffbfb
kylead10 <adiokayode111@gmail.com>
2024-01-19 20:26:06 +0100

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,31 @@
+import React from 'react';
 import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import Header from './components/Header';
-const App = () => {
+import PageNotFound from './components/PageNotFound';
+import ShowStories from './components/ShowStories';
+
+const AppRouter = () => {
   return (
     <BrowserRouter>
       <div className='container'>
         <Header />
+        <Switch>
+          <Route path='/' render={() => <Redirect to='/top' />} exact={true} />
+          <Route
+            path='/:type'
+            render={({ match }) => {
+              const { type } = match.params;
+              if (!['top', 'new', 'best'].includes(type)) {
+                return <Redirect to='/' />;
+              }
+              return <ShowStories type={type} />;
+            }}
+          />
+          <Route component={PageNotFound} />
+        </Switch>
       </div>
     </BrowserRouter>
   );
 };
 
-export default App;
+export default AppRouter;
