f155d77d6d8ddf871005debc0a7e10c255fcb001
kylead10 <adiokayode111@gmail.com>
2024-01-20 22:51:04 +0100

    bug fixing

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,7 +1,28 @@
-import React from 'react';
+import { Route, Routes, Navigate } from 'react-router-dom';
+import Header from '../components/Header';
+import PageNotFound from '../components/PageNotFound';
+import ShowStories from '../components/ShowStories';
 
 const AppRouter = () => {
-  return <div>AppRouter</div>;
+  return (
+    <>
+      <Header /> {/* Place Header outside of Routes if it's global */}
+      <Routes>
+        <Route path='/' element={<Navigate to='/top' replace />} exact />
+        <Route
+          path='/:type'
+          render={({ match }) => {
+            const { type } = match.params;
+            if (!['top', 'new', 'best'].includes(type)) {
+              return <Navigate to='/' />;
+            }
+            return <ShowStories type={type} />;
+          }}
+        />
+        <Route path='*' element={<PageNotFound />} />
+      </Routes>
+    </>
+  );
 };
 
 export default AppRouter;
