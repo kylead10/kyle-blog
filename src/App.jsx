ee0a9a238674ecf305c130b2d3f9dc101bb24583
kylead10 <adiokayode111@gmail.com>
2024-01-20 22:19:12 +0100

    debugging

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,38 @@
-import { Route, Routes, Navigate } from 'react-router-dom';
-import Header from './components/Header';
-import PageNotFound from './components/PageNotFound';
-import ShowStories from './components/ShowStories';
+// import { Route, Routes, Navigate } from 'react-router-dom';
+// import Header from './components/Header';
+// import PageNotFound from './components/PageNotFound';
+// import ShowStories from './components/ShowStories';
+
+// const App = () => {
+//   return (
+//     <>
+//       <div className='container'>
+//         <Header />
+//         <Routes>
+//           <Route path='/' element={<Navigate to='/top' />} exact={true} />
+//           <Route
+//             path='/:type'
+//             render={({ match }) => {
+//               const { type } = match.params;
+//               if (!['top', 'new', 'best'].includes(type)) {
+//                 return <Navigate to='/' />;
+//               }
+//               return <ShowStories type={type} />;
+//             }}
+//           />
+//           <Route component={PageNotFound} />
+//         </Routes>
+//       </div>
+//     </>
+//   );
+// };
+
+// export default App;
+
+import React from 'react';
 
 const App = () => {
-  return (
-    <>
-      <div className='container'>
-        <Header />
-        <Routes>
-          <Route path='/' element={<Navigate to='/top' />} exact={true} />
-          <Route
-            path='/:type'
-            render={({ match }) => {
-              const { type } = match.params;
-              if (!['top', 'new', 'best'].includes(type)) {
-                return <Navigate to='/' />;
-              }
-              return <ShowStories type={type} />;
-            }}
-          />
-          <Route component={PageNotFound} />
-        </Routes>
-      </div>
-    </>
-  );
+  return <div>App</div>;
 };
 
 export default App;
