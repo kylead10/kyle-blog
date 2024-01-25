248194aac3b3c72758a98be1928916b9a1e0a875
kylead10 <adiokayode111@gmail.com>
2024-01-25 17:50:14 +0100

    debug

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,30 +1,30 @@
-import { Route, Routes, Navigate } from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Header from '../components/Header';
 import PageNotFound from '../components/PageNotFound';
 import ShowStories from '../components/ShowStories';
 
 const AppRouter = () => {
   return (
-    <>
-      <Header />
-
-      <Routes>
-        <Route path='/' element={<Navigate to='/top' replace />} exact />
-        <Route
-          path='/:type'
-          render={({ match }) => {
-            const { type } = match.params;
-            console.log('Route type:', type);
-            if (!['top', 'new', 'best'].includes(type)) {
-              return <Navigate to='/' />;
-            }
-            return <ShowStories type={type} />;
-          }}
-        />
-        <Route path='*' element={<PageNotFound />} />
-      </Routes>
-      <div className='container'></div>
-    </>
+    <BrowserRouter>
+      <div className='container'>
+        <Header />
+        <Routes>
+          <Route path='/' element={<Navigate to='/top' replace />} />
+          <Route
+            path='/:type'
+            element={({ match }) => {
+              const { type } = match.params;
+              if (!['top', 'new', 'best'].includes(type)) {
+                return <Navigate to='/' replace />;
+              }
+              return <ShowStories type={type} />;
+            }}
+          />
+          <Route path='*' element={<PageNotFound />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 };
 
