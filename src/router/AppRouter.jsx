c20098aec0f122eec23856e72185ad876f1bcae5
kylead10 <adiokayode111@gmail.com>
2024-01-25 17:50:47 +0100

    bugs

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Header from '../components/Header';
 import PageNotFound from '../components/PageNotFound';
