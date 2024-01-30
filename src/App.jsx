46e09f194ab3c384857f928b46b0ccf7ad596a58
kylead10 <adiokayode111@gmail.com>
2024-01-30 18:46:57 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,34 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import ListPage from './pages/ListPage';
 import PostPage from './pages/PostPage';
+import Navbar from './components/Navbar';
 
 const App = () => {
-  return <></>;
+  return (
+    <Routes>
+      <Route
+        path='/'
+        element={
+          <>
+            {' '}
+            <Navbar />
+            <ListPage />
+          </>
+        }
+      ></Route>
+      <Route
+        path='/:type'
+        element={
+          <>
+            {' '}
+            <Navbar />
+            <ListPage />
+          </>
+        }
+      ></Route>
+      <Route path='/item/:id' element={<PostPage />}></Route>
+    </Routes>
+  );
 };
 
 export default App;
