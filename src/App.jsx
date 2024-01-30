33e0390b7af78d72c22db0bef9deac553edb2ef7
kylead10 <adiokayode111@gmail.com>
2024-01-30 18:43:00 +0100

    imported neccessary modules

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import ListPage from './pages/ListPage';
+import PostPage from './pages/PostPage';
 
 const App = () => {
   return <></>;
